62c35a5f3ca6712172c85288c3b9fab76f9e78f2
alber <alber@DESKTOP-S96FFKC>
2019-04-01 12:22:33 +0200

    Corrección EjercicioPrecio

--- Tema6/src/Ejemplos/EjercicioPrecio.java
+++ Tema6/src/Ejemplos/EjercicioPrecio.java
@@ -2,7 +2,11 @@ package Ejemplos;
 
 public class EjercicioPrecio {
 	public double calcularPrecio(int unidades, double precioUnitario) {
-		return 0.0;
+		double precioTotal;
+		
+		precioTotal = precioUnitario * unidades;
+		
+		return precioTotal;
 		
 	}
 
